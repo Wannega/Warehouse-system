51370400a03aaf01da1709bf8226fdc3dc86c9cd
Wannega <veackrattlehead@gmail.com>
2023-03-02 21:31:28 +0400

    feat: add password reset logic

--- src/components/password-reset/index.tsx
+++ src/components/password-reset/index.tsx
@@ -1,17 +1,72 @@
+'use client'
+
+import { Controller, useForm } from 'react-hook-form'
 import { Button, Input } from '@common'
+import { useResetPasswordMutation } from '@generated'
+import { useDelayedRedirect } from '@utils'
+import { useSearchParams } from 'next/navigation'
 import styled from 'styled-components'
 
+interface FormProps {
+  password: string
+  passwordConfirmation: string
+}
+
 export const PasswordResetPage: React.FC = () => {
+  const [resetPassword] = useResetPasswordMutation()
+  const [redirect] = useDelayedRedirect()
+  const { control, handleSubmit } = useForm<FormProps>()
+  const query = useSearchParams()
+
+  const onReset = async (form: FormProps) => {
+    const { errors } = await resetPassword({
+      variables: {
+        ...form,
+        code: query.get('code') ?? '',
+      },
+    })
+
+    if (!errors) {
+      redirect('/dashboard')
+    }
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onReset)}>
       <Title>Восстановление пароля</Title>
       <p>
         Придумайте сложный пароль, который вы не используете на других
         веб-сайтах
       </p>
-      <Input type={'password'} label="Пароль" />
-      <Input type={'password'} label="Подтверждение пароля" />
-      <Button>Установить пароль</Button>
+      <Controller
+        control={control}
+        name="password"
+        render={({ field: { onChange, onBlur, name } }) => (
+          <Input
+            name={name}
+            required
+            onChange={onChange}
+            onBlur={onBlur}
+            type={'password'}
+            label="Пароль"
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="passwordConfirmation"
+        render={({ field: { onChange, onBlur, name } }) => (
+          <Input
+            name={name}
+            required
+            onChange={onChange}
+            onBlur={onBlur}
+            type={'password'}
+            label="Подтверждение пароля"
+          />
+        )}
+      />
+      <Button type="submit">Установить пароль</Button>
     </Form>
   )
 }
