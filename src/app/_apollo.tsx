06786d39370d18f9593da47e41d9447adb146409
Wannega <veackrattlehead@gmail.com>
2023-03-02 21:16:40 +0400

    feat: add apollo links

--- src/app/_apollo.tsx
+++ src/app/_apollo.tsx
@@ -1,36 +1,62 @@
 import {
   ApolloClient,
+  ApolloLink,
   ApolloProvider,
   createHttpLink,
   InMemoryCache,
 } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { onError } from '@apollo/client/link/error'
+import { LoginMutation } from '@generated'
+import { setUser } from '@store'
 import jsCookie from 'js-cookie'
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_STRAPI_API,
+  uri: process.env.NEXT_PUBLIC_STRAPI_API + '/graphql',
 })
 
-const logoutLink = onError(({ networkError }) => {
-  console.log(networkError)
-})
+// TODO:  CHECK TOKEN VALIDITY PER EACH REQUEST
+// const logoutLink = onError(({ networkError }) => {
+//   console.log(networkError)
+// })
 
-const authLink = setContext((_, { headers }) => {
-  const token = jsCookie.get('bearer-token')
+// const authLink = setContext((_, { headers }) => {
+//   const token = jsCookie.get('bearer-token')
 
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  }
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   }
+// })
+
+const signLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((rawData) => {
+    const searchField = {
+      login: 'login',
+      resetPassword: 'resetPassword',
+    }[operation.operationName]
+
+    if (
+      operation.operationName === 'login' ||
+      operation.operationName === 'resetPassword'
+    ) {
+      // TODO: ADD TYPES
+      const data = rawData.data
+      jsCookie.set('access-token', data[searchField]?.jwt ?? '')
+      setUser(data[searchField]?.user)
+    }
+
+    return rawData
+  })
 })
 
 export const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_STRAPI_API + '/graphql',
-  link: logoutLink.concat(httpLink),
   cache: new InMemoryCache(),
+  ssrMode: true,
+  link: signLink.concat(httpLink),
 })
 
 interface Props {
